23ac4f232c89dd556daae4ea2db7a2376ccb1a2f
lutaibo <lutaibo@gomefinance.com.cn>
2018-09-10 16:08:14 +0800

    添加tabBar和iview组件库

--- src/pages/index/main.js
+++ src/pages/index/main.js
@@ -1,5 +1,14 @@
 import Vue from 'vue'
 import App from './index'
-
 const app = new Vue(App)
 app.$mount()
+
+// 添加 config json
+export default {
+   config:{
+    //在这个地方导入应用组件
+    usingComponents: {
+        "i-button": "../static/iview/button/index"
+    }
+   }
+}
